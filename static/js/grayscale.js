fb54b38171e4c55d8a1165dfc834def243e9f138
Brandon Cathcart <cathcart.brandon@gmail.com>
2017-01-11 16:28:11 -0700

    Started better side-nav for resources page

--- static/js/grayscale.js
+++ static/js/grayscale.js
@@ -16,21 +16,30 @@ function collapseNavbar() {
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
+$(document).ready(function(){
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $(this).closest('.collapse').collapse('toggle');
+    });
+
+
+    //For resources page side-nav
+    $(".sidebar-nav li a").click(function(e) {
+        console.log($(e.target).attr('value'));
     });
 });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $(this).closest('.collapse').collapse('toggle');
-});
 
 // Google Maps Scripts
 var map = null;
