baebd2c9c148888d073ed78ea73f72733c27226f
Brandon Cathcart <cathcart.brandon@gmail.com>
2017-01-26 19:33:28 -0700

    Finished fixing things, tried custom templating function

---
+++ static/data/camps-info.js
@@ -0,0 +1,11 @@
+var campsInfo = {
+    'clay': {
+        'name' : 'Claymation',
+        'icon' : './static/img/icons/clay.png',
+        'desc' : `Make your own clay characters come to life, just like Shaun the Sheep! Participants will  
+                    work with a partner to build their own clay characters, sets and props, develop their own 
+                    script, shoot their movie scene, and then learn to edit and add sounds and special effects 
+                    to complete the whole project. Don't worry parents, there will be plenty of time devoted to 
+                    outdoor games and activities!`
+    }
+}
--- static/js/main.js
+++ static/js/main.js
@@ -43,14 +43,35 @@ $(document).ready(function() {
         $(e.target).css('height', '100px');
     });
 
-    // Closes the Responsive Menu on Menu Item Click
+    /* Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $(this).closest('.collapse').collapse('toggle');
-    });
-
-
-    //For resources page side-nav
-    $(".sidebar-nav li a").click(function(e) {
-        console.log($(e.target).attr('value'));
-    });
+    });*/
+    
+    //makeHeader('clay-header', {'icon':'clay','camp name':'Claymation'});
 });
+
+function makeHeader(targetId, replacements){
+    var temp = "<div class='text-center'>" +
+                    "<img src='../static/img/icons/{{ICON}}.png' style='display:inline; width: 50px;'>" +
+                    "<h1 class='text-center' style='display: inline; line-height: 50px; vertical-align: middle; padding-left:20px; padding-right: 20px;'>{{CAMP NAME}}</h1>" +
+                    "<img src='../static/img/icons/{{ICON}}.png' style='display:inline; width: 50px;'>" +
+                "</div><hr>";
+    temp = btemplater(temp, replacements);
+    document.getElementById(targetId).innerHTML = temp;
+    
+}
+
+
+function btemplater(temp, replacements){
+    var start = 0; var end; var word;
+    while(temp.indexOf('{{', start) > -1){
+        start = temp.indexOf('{{') + 2;
+        end = temp.indexOf('}}');
+        word = temp.substring(start,end).toLowerCase();
+        if(replacements.hasOwnProperty(word)){
+            temp = temp.replace('{{' + word.toUpperCase() + '}}', replacements[word]);
+        }
+    }
+    return temp;  
+}
