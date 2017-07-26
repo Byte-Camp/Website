12d3856704f101686522d1673b66327ff07b04c5
Adar Guy <adarguy10@gmail.com>
2017-07-25 20:54:40 -0700

    made small edits

--- html/payroll/db_utils/connect.php
+++ html/payroll/db_utils/connect.php
@@ -1,5 +1,18 @@
 <?php
 function db_connect() {
+    $host = "bytecamp-db.cjniwxkrwxjh.us-east-2.rds.amazonaws.com";
+    $user = "adminBC";
+    $pass = "2201Chambers";
+    $db = "byteDB";
+    $port = 3306;
+    $connection = mysqli_connect($host, $user, $pass, $db, $port) or die (mysql_error());
+
+    return $connection;
+    
+}
+
+/*
+function db_connect() {
     $host = "localhost";
     $user = "bytecamp_test2";
     $pass = "18888082983Unicorn";
@@ -10,4 +23,5 @@ function db_connect() {
     return $connection;
     
 }
+*/
 ?>
