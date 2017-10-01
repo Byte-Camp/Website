e9e35f86caebb4cd95563d4b009a67da271853b7
Adar Guy <adarguy10@gmail.com>
2017-10-01 11:07:45 -0700

    testing connection...

--- html/payroll/connect/login.php
+++ html/payroll/connect/login.php
@@ -1,4 +1,5 @@
 <?php
+	echo "<script>alert('hi');</script>";
 	include('../db_utils/connect.php');
 	if (!isset($_SESSION)) {
 		@session_start();
@@ -6,21 +7,22 @@
 	if (isset($_POST['submit-login'])) {
 	    $email = stripslashes($_POST['email']);
 	    $password = stripslashes($_POST['password']);
-        //$password = $password . 'somesortofsalt';
+        $password = $password . 'somesortofsalt';
 		$conn = db_connect();
-	    echo "<script>alert('".$email."');<script>";
-	    echo "<script>alert('".$password."');<script>";
+	    echo "<script>alert('".$email."');</script>";
+	    echo "<script>alert('".$password."');</script>";
+
 	    $query = sprintf(
 	        'SELECT * FROM accounts WHERE instructor_email = "%s"',
 	        mysqli_real_escape_string($conn, $email)
 	    );
 	    $result = mysqli_query($conn, $query);
-	    echo "<script>alert('SOMETHING1');<script>";
+	    echo "<script>alert('SOMETHING1');</script>";
 	    if (!$result) {
 	        $error = sprintf('Query Failed: %s', mysql_error());
             echo $error;
         }
-        echo "<script>alert('SOMETHING2');<script>";
+        echo "<script>alert('SOMETHING2');</script>";
         if (mysqli_num_rows($result) > 0) {
         	/*$row_instructor = mysqli_fetch_array($result);
        		$firstname = $row_instructor['first_name'];
