ed66d9ff29dd76eba71a0c7761a5aecd0c11701b
Adar Guy <adarguy10@gmail.com>
2017-10-01 11:23:50 -0700

    testing connection...

--- html/payroll/connect/login.php
+++ html/payroll/connect/login.php
@@ -1,26 +1,18 @@
-<?php include "../../../../../inc/dbinfo.inc"; ?>
 <html>
 <body>
 <h1>Sample page</h1>
 <?php
+	include('../db_utils/connect.php');
+	$connection = db_connect();
+	VerifyEmployeesTable($connection); 
 
-  /* Connect to MySQL and select the database. */
-  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+	/* If input fields are populated, add a row to the Employees table. */
+	$employee_name = htmlentities($_POST['Name']);
+	$employee_address = htmlentities($_POST['Address']);
 
-  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
-
-  $database = mysqli_select_db($connection, DB_DATABASE);
-
-  /* Ensure that the Employees table exists. */
-  VerifyEmployeesTable($connection, DB_DATABASE); 
-
-  /* If input fields are populated, add a row to the Employees table. */
-  $employee_name = htmlentities($_POST['Name']);
-  $employee_address = htmlentities($_POST['Address']);
-
-  if (strlen($employee_name) || strlen($employee_address)) {
-    AddEmployee($connection, $employee_name, $employee_address);
-  }
+	if (strlen($employee_name) || strlen($employee_address)) {
+	AddEmployee($connection, $employee_name, $employee_address);
+	}
 ?>
 
 <!-- Input form -->
@@ -67,59 +59,9 @@ while($query_data = mysqli_fetch_row($result)) {
 
 </table>
 
-<!-- Clean up. -->
-<?php
-
-  mysqli_free_result($result);
-  mysqli_close($connection);
-
-?>
-
 </body>
 </html>
 
-
-<?php
-
-/* Add an employee to the table. */
-function AddEmployee($connection, $name, $address) {
-   $n = mysqli_real_escape_string($connection, $name);
-   $a = mysqli_real_escape_string($connection, $address);
-
-   $query = "INSERT INTO `Employees` (`Name`, `Address`) VALUES ('$n', '$a');";
-
-   if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
-}
-
-/* Check whether the table exists and, if not, create it. */
-function VerifyEmployeesTable($connection, $dbName) {
-  if(!TableExists("Employees", $connection, $dbName)) 
-  { 
-     $query = "CREATE TABLE `Employees` (
-         `ID` int(11) NOT NULL AUTO_INCREMENT,
-         `Name` varchar(45) DEFAULT NULL,
-         `Address` varchar(90) DEFAULT NULL,
-         PRIMARY KEY (`ID`),
-         UNIQUE KEY `ID_UNIQUE` (`ID`)
-       ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1";
-
-     if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
-  }
-}
-
-/* Check for the existence of a table. */
-function TableExists($tableName, $connection, $dbName) {
-  $t = mysqli_real_escape_string($connection, $tableName);
-  $d = mysqli_real_escape_string($connection, $dbName);
-
-  $checktable = mysqli_query($connection, 
-      "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
-
-  if(mysqli_num_rows($checktable) > 0) return true;
-
-  return false;
-}
-?>
 <?php
 /*
 	echo "<script>alert('hi');</script>";
--- html/payroll/db_utils/connect.php
+++ html/payroll/db_utils/connect.php
@@ -8,6 +8,49 @@ function db_connect() {
     return $connection;
 }
 
+/* Check whether the table exists and, if not, create it. */
+function VerifyEmployeesTable($connection) {
+  if(!TableExists("Employees", $connection, DB_DATABASE)) 
+  { 
+     $query = "CREATE TABLE `Employees` (
+         `ID` int(11) NOT NULL AUTO_INCREMENT,
+         `Name` varchar(45) DEFAULT NULL,
+         `Address` varchar(90) DEFAULT NULL,
+         PRIMARY KEY (`ID`),
+         UNIQUE KEY `ID_UNIQUE` (`ID`)
+       ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1";
+
+     if(!mysqli_query($connection, $query)) echo("<p>Error creating table.</p>");
+  }
+  else
+  {
+    echo "<script>alert('SOMETHING1');</script>";
+  }
+}
+
+/* Check for the existence of a table. */
+function TableExists($tableName, $connection, $dbName) {
+  $t = mysqli_real_escape_string($connection, $tableName);
+  $d = mysqli_real_escape_string($connection, $dbName);
+
+  $checktable = mysqli_query($connection, 
+      "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = '$t' AND TABLE_SCHEMA = '$d'");
+
+  if(mysqli_num_rows($checktable) > 0) return true;
+
+  return false;
+}
+
+/* Add an employee to the table. */
+function AddEmployee($connection, $name, $address) {
+   $n = mysqli_real_escape_string($connection, $name);
+   $a = mysqli_real_escape_string($connection, $address);
+
+   $query = "INSERT INTO `Employees` (`Name`, `Address`) VALUES ('$n', '$a');";
+
+   if(!mysqli_query($connection, $query)) echo("<p>Error adding employee data.</p>");
+}
+
 /*
 function db_connect() {
     $host = "localhost";
@@ -21,4 +64,12 @@ function db_connect() {
     
 }
 */
+?>
+
+<!-- Clean up. -->
+<?php
+
+  mysqli_free_result($result);
+  mysqli_close($connection);
+
 ?>
